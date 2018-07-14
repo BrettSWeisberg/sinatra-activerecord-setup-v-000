50ab1087c62b0582c01b559f649de8b82d4f9094
Brett Weisberg <weisberg.brett@gmail.com>
2018-07-14 18:54:28 +0000

    automatically backed up by learn

--- db/migrate/20180714184420_create_dogs.rb
+++ db/migrate/20180714184420_create_dogs.rb
@@ -1,8 +1,10 @@
 class CreateDogs < ActiveRecord::Migration
-  def up
-  end
+  class CreateDogs < ActiveRecord::Migration
+    def change
+      create_table :dogs do |t|
+        t.string :name
+        t.string :breed
+      end
+    end
 
-  def down
   end
-  
-end
